bf56a4fd9813881076a9b608fa27363967a654a7
Argonaut <gaming.genos1729@gmail.com>
2021-06-08 10:30:08 +0530

    Update music.py

--- cogs1/music.py
+++ cogs1/music.py
@@ -9,6 +9,10 @@ import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from pytube import Playlist
+# import re
+# import urllib.request
+# from bs4 import BeautifulSoup
+# import azapi
 
 m = DiscordUtils.Music()
 
@@ -19,6 +23,30 @@ cid = vari['spotipyid']
 ctoken = vari['spotipytoken']
 apikey = vari['musixmatchkey']
 client_cred = SpotifyClientCredentials(client_id=cid, client_secret=ctoken)
+
+# def get_lyrics(artist,song_title): 
+#     artist = artist.lower() 
+#     song_title = song_title.lower() 
+#     # remove all except alphanumeric characters from artist and song_title 
+#     artist = re.sub('[^A-Za-z0-9]+', "", artist) 
+#     song_title = re.sub('[^A-Za-z0-9]+', "", song_title) 
+#     if artist.startswith("the"):    # remove starting 'the' from artist e.g. the who -> who 
+#         artist = artist[3:] 
+#     url = "http://azlyrics.com/lyrics/"+artist+"/"+song_title+".html" 
+     
+#     try: 
+#         content = urllib.request.urlopen(url).read() 
+#         soup = BeautifulSoup(content, 'html.parser') 
+#         lyrics = str(soup) 
+#         # lyrics lies between up_partition and down_partition 
+#         up_partition = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->' 
+#         down_partition = '<!-- MxM banner -->' 
+#         lyrics = lyrics.split(up_partition)[1] 
+#         lyrics = lyrics.split(down_partition)[0] 
+#         lyrics = lyrics.replace('<br>','').replace('</br>','').replace('</div>','').strip().replace('<br/>','')
+#         return lyrics 
+#     except Exception as e: 
+#         return "Exception occurred \n" +str(e)
 
 class music(commands.Cog):
 
