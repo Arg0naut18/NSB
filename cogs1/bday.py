e8382aea0ce69d0ede6d2847e02b71e58e048350
Argonaut <gaming.genos1729@gmail.com>
2021-06-17 19:57:12 +0530

    added bday shit and adding economy

--- cogs1/bday.py
+++ cogs1/bday.py
@@ -1,10 +1,8 @@
 import discord
-import random
-from discord.ext import commands
-from discord.utils import tasks
+from discord.ext import commands, tasks
 import json
 from datetime import date
-
+import calendar
 
 class bday(commands.Cog):
     def __init__(self, bot):
@@ -14,19 +12,54 @@ class bday(commands.Cog):
     async def setbday(self, ctx, member: discord.Member, bday):
         with open(r'./bday/bdays.json', 'r') as k:
             bdays = json.load(k)
-        bdays[str(ctx.guild.id)][str(member.id)] = f'{bday}'
+        try:
+            bdays[str(ctx.guild.id)][str(member.id)] = f'{bday}'
+        except KeyError:
+            bdays[str(ctx.guild.id)] = {}
+            bdays[str(ctx.guild.id)][str(member.id)] = f'{bday}'
         with open(r'./bday/bdays.json', 'w') as j:
             json.dump(bdays, j, indent=4)
         await ctx.send(f"Birthday of {member.display_name} set on {bday}")
-
-    @tasks.loop(hours=24)
-    async def check(self):
-        today = date.today()
-        day, month = today.day, today.month
-        today = day+'-'+month
+    
+    @commands.command()
+    async def showbdays(self, ctx, server: discord.Guild=None):
+        server = ctx.guild if not server else server
         with open(r'./bday/bdays.json', 'r') as j:
             bdays = json.load(j)
-        
+        msg = ''
+        for user_id in bdays[str(server.id)].keys():
+            date = bdays[str(server.id)][user_id]
+            day, month = date.split('-')
+            day = int(day)
+            month = int(month)
+            month = calendar.month_name[month]
+            date = str(day) + ' ' +month
+            member = await self.bot.fetch_user(user_id)
+            msg = msg + (f"**{member.mention}** => **{date}**\n")
+        bdayembed = discord.Embed(title=f"Birthdays of members from {server.name}", description=msg, color=0x00FF00)
+        await ctx.send(embed=bdayembed)
+
+    # @tasks.loop(minutes=1)
+    # async def check(self):
+    #     today = date.today()
+    #     day, month = today.day, today.month
+    #     if day<10:
+    #         daystr = '0'+str(day)
+    #     else:
+    #         daystr = day
+    #     if month<10:
+    #         monthstr = '0'+str(month)
+    #     else:
+    #         monthstr = month
+    #     todaydate = daystr+'-'+monthstr
+    #     channel = discord.utils.get(self.bot.get_all_channels(), id=855031679281397761)
+    #     with open(r'./bday/bdays.json', 'r') as j:
+    #         bdays = json.load(j)
+    #     for server_id in bdays:
+    #         for user_id in server_id:
+    #             if bdays[str(server_id)][str(user_id)] == todaydate:
+    #                 user = discord.utils.get(self.bot.get_all_members(), id=user_id)
+    #                 await channel.send(f"A very big Happy Birthday to you {user.mention}.")
 
 def setup(bot):
     bot.add_cog(bday(bot))
