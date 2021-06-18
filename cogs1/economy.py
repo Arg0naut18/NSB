67ad05922abea4cc85adbb93e5c76d5832ac30f8
Argonaut <gaming.genos1729@gmail.com>
2021-06-18 22:22:58 +0530

    Update economy.py

--- cogs1/economy.py
+++ cogs1/economy.py
@@ -45,20 +45,21 @@ class economy(commands.Cog):
         emb = discord.Embed(title=f"{member.name}'s Balance", color=0x00FF00)
         emb.add_field(name=":money_with_wings: Wallet balance", value=f"`{wallet_money}`", inline=False)
         emb.add_field(name=":bank: Bank balance", value=f"`{bank_money}`", inline=False)
+        emb.set_footer(text=f"Invoked by: {member.name}", icon_url=member.avatar_url)
         await ctx.send(embed=emb)
 
     @commands.command()
     async def beg(self, ctx):
         await open_account(ctx.author)
         users = await get_account_data()
-        money = random.randrange(100)
+        money = random.randrange(500)
         if money != 0:
             users[str(ctx.author.id)]["wallet"] += money
             with open(r'./bank/bank.json', 'w') as f:
                 json.dump(users, f, indent=4)
-            await ctx.send(f"Someone just gave you :coin:`{money}`! Congrats :tada:!")
+            await ctx.send(f"Someone just gave you :coin:`{money}`! Congrats <a:partygif:855108791532388422>!")
         else:
-            await ctx.send(f"Better luck next time. <:sadcrypeace:855109459852656690>")
+            await ctx.send(f"Better luck next time. <a:sadcrypeace:855109649962369054>")
 
     @commands.command()
     async def deposit(self, ctx, amount=None):
@@ -78,7 +79,26 @@ class economy(commands.Cog):
         await update_bank_data(ctx.author, int(amount), "bank")
         await update_bank_data(ctx.author, -1*int(amount))
         await ctx.send(f"You just deposited :coin:{amount}!")
-    
+
+    @commands.command()
+    async def withdraw(self, ctx, amount=None):
+        if amount is None:
+            await ctx.send("You need to mention the amount you wanna deposit.")
+            return
+        await open_account(ctx.author)
+        bal = await update_bank_data(ctx.author)
+        if amount == "all" or amount == "max":
+            amount = bal[1]
+        if int(amount)>bal[1]:
+            await ctx.send("You don't have enough money.")
+            return
+        if int(amount)<0:
+            await ctx.send("The amount can't be negative.")
+            return
+        await update_bank_data(ctx.author, int(amount))
+        await update_bank_data(ctx.author, -1*int(amount), "bank")
+        await ctx.send(f"You just withdrew :coin:{amount}!")
+
     @commands.command()
     async def give(self, ctx, member: discord.Member=None, amount=None):
         if amount is None:
@@ -101,26 +121,7 @@ class economy(commands.Cog):
         await update_bank_data(member, int(amount))
         await update_bank_data(ctx.author, -1*int(amount))
         await ctx.send(f"You just gave :coin:{amount} to {member.mention}! What a generous lad.")
-
-    @commands.command()
-    async def withdraw(self, ctx, amount=None):
-        if amount is None:
-            await ctx.send("You need to mention the amount you wanna deposit.")
-            return
-        await open_account(ctx.author)
-        bal = await update_bank_data(ctx.author)
-        if amount == "all" or amount == "max":
-            amount = bal[1]
-        if int(amount)>bal[1]:
-            await ctx.send("You don't have enough money.")
-            return
-        if int(amount)<0:
-            await ctx.send("The amount can't be negative.")
-            return
-        await update_bank_data(ctx.author, int(amount))
-        await update_bank_data(ctx.author, -1*int(amount), "bank")
-        await ctx.send(f"You just withdrew :coin:{amount}!")
-
+    
     @commands.command()
     async def rob(self, ctx, member: discord.Member=None):
         if member is None:
@@ -136,7 +137,7 @@ class economy(commands.Cog):
         if membal[0]<100:
             await ctx.send("It's not worth it man.")
             return
-        chance = random.randrange(0, 2)
+        chance = random.randrange(0,2)
         if chance==1:
             amount = random.randrange(0, membal[0])
             await update_bank_data(member, -1*int(amount))
@@ -144,7 +145,7 @@ class economy(commands.Cog):
             await ctx.send(f"You just robbed :coin:{amount} from {member.mention}! Poor lad.")
         else:
             await update_bank_data(ctx.author, -50)
-            await ctx.send(f"Failed to rob {member}. You lost :coin:50!")
+            await ctx.send(f"Failed to rob {member.mention}. You lost :coin:50!")
 
 def setup(bot):
     bot.add_cog(economy(bot))
