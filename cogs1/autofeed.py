cbef3d491a76eb63bbb84034e31c892931592ca4
Argonaut <gaming.genos1729@gmail.com>
2021-07-03 13:40:55 +0530

    Update autofeed.py

--- cogs1/autofeed.py
+++ cogs1/autofeed.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 class autofeed(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.my_task.start()
 
     @tasks.loop(hours=24)
     async def my_task(self):
@@ -14,8 +15,8 @@ class autofeed(commands.Cog):
 
     @my_task.before_loop
     async def before_my_task(self):
-        hour = 12
-        minute = 20
+        hour = 13
+        minute = 45
         await self.bot.wait_until_ready()
         now = datetime.now()
         future = datetime.datetime(now.year, now.month, now.day, hour, minute)
