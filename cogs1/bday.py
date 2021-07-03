063113839ed1d3074fec61dafb0c09bdfb3d7b36
Argonaut <gaming.genos1729@gmail.com>
2021-07-03 20:54:22 +0530

    Update bday.py

--- cogs1/bday.py
+++ cogs1/bday.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import json
 from datetime import date
 import calendar
+import random
 
 class bday(commands.Cog):
     def __init__(self, bot):
@@ -61,7 +62,17 @@ class bday(commands.Cog):
             for user_id in bdays[str(server_id)]:
                 if bdays[server_id][user_id] == todaydate:
                     user = discord.utils.get(self.bot.get_all_members(), id=int(user_id))
-                    await channel.send(f"A very big Happy Birthday to you {user.mention}.")
+                    await channel.send(f"A very big Happy Birthday 🎂 to you {user.mention} from <@!436844058217021441> and NSB community. <a:partyconfetti:860885019898937374>")
+                    urls = ['https://media.tenor.com/images/e37ae589afa0cffe7c9957bee26e36cc/tenor.gif',
+                    'https://media.tenor.com/images/72fead26968d18a5846f02298dacb3b3/tenor.gif',
+                    'https://media.tenor.com/images/ee1cd9269f1872a5eb31cef9b86a20cd/tenor.gif',
+                    'https://media.tenor.com/images/ac4f49e01b8d289c16271e2187ee62d8/tenor.gif',
+                    'https://i.pinimg.com/originals/4f/b9/96/4fb996524beabfa60c7ca4394057bbc9.gif',
+                    'https://media.tenor.com/images/25d627f3e0f09fe712f8b9fd4bd675cb/tenor.gif',
+                    'https://media.tenor.com/images/0b6f0b738d777f1f393492918ef94eda/tenor.gif']
+                    embed = discord.Embed(title=(f'__***NotSoBasicBot***__ is wishing **Happy Birthday** to __***{user.name}***__') ,color=0x00ff00)
+                    embed.set_image(url=random.choice(urls))
+                    await channel.send(embed=embed)
     
 def setup(bot):
     bot.add_cog(bday(bot))
