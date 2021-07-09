73766e9d124266b8e1b62534aa26d4a0f922ed9b
Argonaut <gaming.genos1729@gmail.com>
2021-07-09 14:04:09 +0530

    Update welcome.py

--- cogs1/welcome.py
+++ cogs1/welcome.py
@@ -5,10 +5,12 @@ from PIL import Image, ImageFont, ImageDraw, ImageOps, ImageFilter, ImageChops
 from io import BytesIO
 import json
 
+
 async def get_servers():
     with open(r'./welcome/welcome.json') as f:
         servers = json.load(f)
     return servers
+
 
 async def open_account(user):
     with open(r'./bank/bank.json', 'r') as f:
@@ -26,10 +28,12 @@ async def open_account(user):
         json.dump(users, f, indent=4)
     return True
 
+
 async def get_account_data():
     with open(r"./bank/bank.json", 'rb') as j:
         bank = json.load(j)
     return bank
+
 
 class welcome(commands.Cog):
 
@@ -39,6 +43,12 @@ class welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         servers = await get_servers()
+        if member.guild.id == 743741348578066442:
+            await open_account(member)
+            users = await get_account_data()
+            users[str(member.id)]["multiplier"] += 1
+            with open(r"./bank/bank.json", 'w') as bank:
+                json.dump(users, bank, indent=4)
         if str(member.guild.id) in servers:
             server_name = member.guild.name
             welcome_channel = discord.utils.get(member.guild.text_channels, id=servers[str(member.guild.id)]["welcomechannel"])
@@ -64,33 +74,33 @@ class welcome(commands.Cog):
             draw = ImageDraw.Draw(mask)
             draw.ellipse((0, 0) + bigsize, fill=255)
             mask = mask.resize(pfp.size, Image.ANTIALIAS)
-            #mask = ImageChops.darker(mask, pfp.split()[-1])
+            # mask = ImageChops.darker(mask, pfp.split()[-1])
             pfp.putalpha(mask)
             pfp.crop((0, 0)+bigsize)
             pfp = pfp.resize((servers[str(member.guild.id)]["sizes"]["pfpsize"], servers[str(member.guild.id)]["sizes"]["pfpsize"])).convert("RGBA")
-            #pfp = mask_circle_transparent(pfp, 1)
+            # pfp = mask_circle_transparent(pfp, 1)
             layout.paste(pfp, (servers[str(member.guild.id)]["coordinates"]["pfp"][0], servers[str(member.guild.id)]["coordinates"]["pfp"][1]), pfp)
             draw = ImageDraw.Draw(layout)
             font1 = ImageFont.truetype("./MPLUSRounded1c-Medium.ttf", servers[str(member.guild.id)]["sizes"]["font1"])
             font2 = ImageFont.truetype("./MPLUSRounded1c-Bold.ttf", servers[str(member.guild.id)]["sizes"]["font2"])
             font3 = ImageFont.truetype("./BullettoKilla.ttf", servers[str(member.guild.id)]["sizes"]["font3"])
             imgdesc = f"{member}"
-            #w, h = draw.textsize(imgdesc)
+            # w, h = draw.textsize(imgdesc)
             if "belowmessage" in servers[str(member.guild.id)]:
                 imgmssg = servers[str(member.guild.id)]["belowmessage"].format(member.guild.name)
-                #imgmssg = servers[str(member.guild.id)]["belowmessage"].format("Ɖɨʋɨռɛ")
-                draw.text((45, 217), imgmssg, (servers[str(member.guild.id)]["color"][0], servers[str(member.guild.id)]["color"][1], servers[str(member.guild.id)]["color"][2]), font2)
+                # imgmssg = servers[str(member.guild.id)]["belowmessage"].format("Ɖɨʋɨռɛ")
+                draw.text((servers[str(member.guild.id)]["coordinates"]["belowmessage"][0], servers[str(member.guild.id)]["coordinates"]["belowmessage"][1]), imgmssg, (servers[str(member.guild.id)]["color"][0], servers[str(member.guild.id)]["color"][1], servers[str(member.guild.id)]["color"][2]), font2)
             draw.text((servers[str(member.guild.id)]["coordinates"]["membername"][0], servers[str(member.guild.id)]["coordinates"]["membername"][1]), imgdesc, (servers[str(member.guild.id)]["color"][0], servers[str(member.guild.id)]["color"][1], servers[str(member.guild.id)]["color"][2]), font1)
             draw.text((servers[str(member.guild.id)]["coordinates"]["welcome"][0], servers[str(member.guild.id)]["coordinates"]["welcome"][1]), "WELCOME!", (servers[str(member.guild.id)]["color"][0], servers[str(member.guild.id)]["color"][1], servers[str(member.guild.id)]["color"][2]), font3)
             layout.save(r"./welcome/welcomeedit.jpg")
-            file=discord.File(r"./welcome/welcomeedit.jpg", filename="image.png")
+            file = discord.File(r"./welcome/welcomeedit.jpg", filename="image.png")
             await welcome_channel.send(f"{member.mention}")
             welcomeEmbed = discord.Embed(description=mssg, color=0x00FF00)
             welcomeEmbed.set_author(name=f"Welcome {member.display_name}!", icon_url=member.avatar_url)
             welcomeEmbed.set_image(url="attachment://image.png")
             await welcome_channel.send(file=file, embed=welcomeEmbed)
             if role is not None:
-                role_ = discord.utils.get(member.guild.roles, id = 711447121391255602)
+                role_ = discord.utils.get(member.guild.roles, id=int(role))
                 await member.add_roles(role_)
         
     @commands.Cog.listener()
@@ -105,7 +115,7 @@ class welcome(commands.Cog):
         if member.guild.id == 743741348578066442:
             await open_account(member)
             users = await get_account_data()
-            users[str(member.id)]["multiplier"]=1
+            users[str(member.id)]["multiplier"] -= 1
             with open(r"./bank/bank.json", 'w') as bank:
                 json.dump(users, bank, indent=4)
     
@@ -113,7 +123,7 @@ class welcome(commands.Cog):
     async def intro(self, ctx):
         dev_name = "Argonaut#6921"
         dev_id = 436844058217021441
-        #camp_name = "Ɖɨʋɨռɛ"
+        # camp_name = "Ɖɨʋɨռɛ"
         dev = await self.bot.fetch_user(dev_id)
         msg = f"Thanks for you're concern. I am a \
 bot developed by {dev} to help you out or entertain you in your daily busy lives."
@@ -133,7 +143,8 @@ bot developed by {dev} to help you out or entertain you in your daily busy lives
             if str(user.id) in users:
                 users[str(user.id)]["multiplier"]=2
         with open(r"./bank/bank.json", 'w') as bank:
-                json.dump(users, bank, indent=4)
+            json.dump(users, bank, indent=4)
+
 
 def setup(bot):
     bot.add_cog(welcome(bot))
