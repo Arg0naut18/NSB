3d494a1f09491911aae552b69ea726099779d823
Argonaut <gaming.genos1729@gmail.com>
2021-07-20 00:47:58 +0530

    Update translate.py

--- cogs1/translate.py
+++ cogs1/translate.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from googletrans import Translator, LANGUAGES
 import random
+import asyncio
 
 color = [15158332, 3066993, 10181046, 3447003, 1752220, 15844367]
 
@@ -18,7 +19,39 @@ class trans(commands.Cog):
         embed = discord.Embed(title=" ", color=color_main)
         embed.add_field(name="Translation", value=translation.text, inline=False)
         embed.add_field(name="Source Lang.", value=LANGUAGES[translation.src].title(), inline=False)
-        await ctx.reply(embed=embed)
+        t = await ctx.reply(embed=embed)
+        await t.add_reaction("<a:wrong:859371475482705920>")
+        def check1(reaction, user):
+            if not user.bot:
+                return reaction.message.id == t.id and str(reaction.emoji) == "<a:wrong:859371475482705920>"
+            else:
+                return False
+        try:
+            await self.bot.wait_for("reaction_add", timeout=30, check=check1)
+        except TimeoutError:
+            return
+        else:
+            await ctx.send("Enter the language you think the sentence is in:")
+            def check2(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+            try:
+                lang = await self.bot.wait_for("message", timeout=30, check=check2)
+            except:
+                pass
+            else:
+                translator = Translator()
+                try:
+                    translation = translator.translate(sentence, src=lang.content.lower())
+                except:
+                    await ctx.send("Language not supported!")
+                    return
+                color_main = color[random.randint(0, len(color)-1)]
+                embed = discord.Embed(title=" ", color=color_main)
+                embed.add_field(name="Translation", value=translation.text, inline=False)
+                embed.add_field(name="Source Lang.", value=LANGUAGES[translation.src].title(), inline=False)
+                t = await ctx.reply(embed=embed)
+
+
 
 def setup(bot):
     bot.add_cog(trans(bot))
