27b6470359c46f08e3abf7e8b892c2677339c6e2
Argonaut <gaming.genos1729@gmail.com>
2021-08-13 15:49:32 +0530

    Update mcraft.py

--- cogs1/mcraft.py
+++ cogs1/mcraft.py
@@ -1,6 +1,8 @@
+#import gevent.monkey
+#gevent.monkey.patch_all()
 import discord
 from discord.ext import commands
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
 class mcraft(commands.Cog):
@@ -8,24 +10,27 @@ class mcraft(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['tame', 'iaf', 'fireandice'])
-    async def iceandfire(self, ctx, query=None):
+    async def iceandfire(self, ctx, *, query=None):
         if query is None:
             return
         try:
             if " " in query:
-                query = query.replace(" ", "_")
-            r = requests.get(url=f"https://ice-and-fire-mod.fandom.com/wiki/{query.title()}")
-        except:
+                q = query.replace(" ", "_")
+            else:
+                q = query
+            r = urllib.request.urlopen(f"https://ice-and-fire-mod.fandom.com/wiki/{q.title()}")
+        except Exception as e:
             await ctx.send("`Entity not found!`")
+            print(e)
             return
-        soup = BeautifulSoup(r.content, "html.parser")
+        soup = BeautifulSoup(r, "html.parser")
         description = soup.find(property="og:description")
         desc = description.prettify()[15:-29]
         Description = desc.split(".")[0]+"."
-        tameembed = discord.Embed(title=f"{query.title()}", color="0x00ff00")
+        tameembed = discord.Embed(title=f"{query.title()}", color=0x00ff00)
         tameembed.add_field(name="Description", value=Description, inline=False)
         try:
-            taming = soup.find_all("p", string=lambda text: text and "tamed" in text.lower())
+            taming = soup.find_all("p", string=lambda text: text and ("tamed" in text.lower() or "befriended" in text.lower()))
             tame = str(taming[0])[3:-4]
             tameembed.add_field(name="Taming", value=tame, inline=False)
         except:
@@ -33,7 +38,7 @@ class mcraft(commands.Cog):
         imagethumb = soup.find("a", class_="image image-thumbnail")
         imageurlstart = str(imagethumb).find("href=")
         imageurlend = str(imagethumb).find(" title")
-        imageurl = str(imagethumb)[imageurlstart+5:imageurlend]
+        imageurl = str(imagethumb)[imageurlstart+6:imageurlend-1]
         tameembed.set_image(url=imageurl)
         await ctx.send(embed=tameembed)
             
