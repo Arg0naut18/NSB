b406f14625f0f58400b8fee7fd96640a446cf52f
Argonaut <gaming.genos1729@gmail.com>
2021-08-14 15:43:02 +0530

    Update mcraft.py

--- cogs1/mcraft.py
+++ cogs1/mcraft.py
@@ -41,6 +41,39 @@ class mcraft(commands.Cog):
         imageurl = str(imagethumb)[imageurlstart+6:imageurlend-1]
         tameembed.set_image(url=imageurl)
         await ctx.send(embed=tameembed)
+        
+    @commands.command(aliases=['minecraftwiki', 'mcraftwiki'])
+    async def mcraft(self, ctx, *, query=None):
+        if query is None:
+            return
+        try:
+            if " " in query:
+                q = query.replace(" ", "_")
+            else:
+                q = query
+            r = urllib.request.urlopen(f"https://minecraft.fandom.com/wiki/{q.title()}")
+        except Exception as e:
+            await ctx.send("`Entity not found!`")
+            print(e)
+            return
+        soup = BeautifulSoup(r, "html.parser")
+        description = soup.find(property="og:description")
+        desc = description.prettify()[15:-29]
+        Description = desc.split(".")[0]+"."
+        tameembed = discord.Embed(title=f"{query.title()}", color=0x00ff00)
+        tameembed.add_field(name="Description", value=Description, inline=False)
+        try:
+            taming = soup.find_all("p", string=lambda text: text and ("tamed" in text.lower() or "befriended" in text.lower()))
+            tame = str(taming[0])[3:-4]
+            tameembed.add_field(name="Taming", value=tame, inline=False)
+        except:
+            pass
+        imagethumb = soup.find("a", class_="image")
+        imageurlstart = str(imagethumb).find("href=")
+        imageurlend = str(imagethumb).find(" title")
+        imageurl = str(imagethumb)[imageurlstart+6:imageurlend-1]
+        tameembed.set_image(url=imageurl)
+        await ctx.send(embed=tameembed)
             
 def setup(bot):
     bot.add_cog(mcraft(bot))
